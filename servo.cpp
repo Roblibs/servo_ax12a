f708d9b5347851e156e3c4d750a2fa05e7fa7b41
wassfila <wassim.filali@gmail.com>
2017-07-01 21:08:10 +0200

    added plugin reading torque from msg

---
+++ servo.cpp
@@ -0,0 +1,71 @@
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <stdio.h>
+#include <boost/algorithm/string/replace.hpp>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/gazebo_client.hh>
+
+namespace igm = ignition::math;
+namespace gzp = gazebo::physics;
+namespace gzt = gazebo::transport;
+
+namespace gazebo
+{
+  class ModelServo : public ModelPlugin
+  {
+    private:
+    physics::ModelPtr model;
+    event::ConnectionPtr updateConnection;
+
+    gazebo::transport::NodePtr node;
+    //gzt::node snode;
+    std::string topic;
+
+    public:
+    double     Torque;
+    igm::Angle Reference;
+
+    public:
+
+    void Load(physics::ModelPtr _parent, sdf::ElementPtr sdf)
+    {
+      // Store the pointer to the model
+      model = _parent;
+      printf("Hello Servo Motor : AX-12A\n");
+      // Listen to the update event. This event is broadcast every
+      // simulation iteration.
+      updateConnection = event::Events::ConnectWorldUpdateBegin(
+          boost::bind(&ModelServo::OnUpdate, this, _1));
+
+      node = gzt::NodePtr(new gzt::Node());
+      node->Init();
+      std::cout << "Servo Node Initialized " << std::endl;
+
+      //topic = sdf->Get<std::string>("topic");
+      node->Subscribe("servo_ax12a_topic",&ModelServo::OnTopicReception,this);
+      std::cout << "Subscribed to topic " << topic << std::endl;
+
+
+      Torque = 0;
+    }
+
+    // Called by the world update start event
+    void OnUpdate(const common::UpdateInfo & /*_info*/)
+    {
+      model->GetJoint("j_Body_Wheel")->SetForce(0,Torque);
+    }
+
+    void OnTopicReception(ConstIntPtr& msg)
+    {
+      Torque = (double)msg->data()/1000;
+      std::cout << "Message> Torque set to : " << Torque << std::endl;
+    }
+
+  };
+
+  // Register this plugin with the simulator
+  GZ_REGISTER_MODEL_PLUGIN(ModelServo)
+}
